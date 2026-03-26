0e1d3780c9557ed07b462d436dadc8cbaa8ebba0
srjuninn <ryanferreiradasilva2006@gmail.com>
2026-03-26 11:12:26 -0300

    wip: add js style of task item

---
+++ src/styles/TaskItemStyles.js
@@ -0,0 +1,20 @@
+import { StyleSheet } from 'react-native'
+import { STATUS_BAR_HEIGHT,
+         PRIMARY_COLOR,
+         ACCENT_COLOR_YELLOW,
+         ACCENT_COLOR_RED,
+         TEXT_COLOR_DARK,
+         TEXT_COLOR_LIGHT,
+         TEXT_COLOR_MEDIUM,
+         TEXT_COLOR_DISABLED,
+         BACKGROUND_COLOR_LIGHT,
+         BACKGROUND_COLOR_WHITE
+ } from '../constant/styles'
+
+  export default StyleSheet.create({
+    taskItem:{
+        backgroundColor: BACKGROUND_COLOR_WHITE,
+        paddingVertical: 12,
+        paddingHorizontal: 15,
+    }
+  })
